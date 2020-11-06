fe840ec9339e913e0f9080972e1ea2ff6bb9f7e2
SANTOSH KUMAR PRAJAPATI <santosh18prajapati@gmail.com>
2020-11-06 23:39:45 +0530

    Add files via upload

---
+++ receiver.js
@@ -0,0 +1,24 @@
+const amqp = require('amqplib/callback_api');
+
+// Step 1: Create Connection
+amqp.connect('amqp://localhost', (connError, connection) => {
+    if (connError) {
+        throw connError;
+    }
+    // Step 2: Create Channel
+    connection.createChannel((channelError, channel) => {
+        if (channelError) {
+            throw channelError;
+        }
+        // Step 3: Assert Queue
+        const QUEUE = 'codingtestqueue'
+        channel.assertQueue(QUEUE);
+        // Step 4: Receive Messages
+        channel.consume(QUEUE, (msg) => {
+            console.log(`Message received: ${msg.content.toString()}`)
+        }, {
+            //acknowledment means massge would be deleted form queue after recieving
+            noAck: true
+        })
+    })
+})
---
+++ sender.js
@@ -0,0 +1,21 @@
+const amqp = require('amqplib/callback_api');
+
+// Step 1: Create Connection
+amqp.connect('amqp://localhost', (connError, connection) => {
+    if (connError) {
+        throw connError;
+    }
+    // Step 2: Create Channel
+    connection.createChannel((channelError, channel) => {
+        if (channelError) {
+            throw channelError;
+            
+        }
+        // Step 3: Assert Queue
+        const QUEUE = 'codingtestqueue'
+        channel.assertQueue(QUEUE);
+        // Step 4: Send message to queue
+        channel.sendToQueue(QUEUE, Buffer.from('hello Santosh it is  coding time'));
+        console.log(`Message send ${QUEUE}`);
+    })
+})
